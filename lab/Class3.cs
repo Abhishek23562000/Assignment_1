0bb982f9ffe6efd95b29bb32bea28d4ff25ea798
ay899 <ay899@LAPTOP-L7SB0FT1>
2023-08-28 08:35:53 +0530

    Add project files.

---
+++ lab/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    partial class Assign
+    {
+        public void largest()
+        {
+            //3.Develop a C# program that finds the largest element in an integer array using a loop and if-else statements.
+
+            int[] arr = { 1, 2, 3, 4 };
+            int s = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        s = arr[i];
+                    }
+                }
+            }
+            Console.WriteLine("Largest Element : " + s);
+        }
+    }
+
+}
---
+++ lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assign obj1 = new Assign();
+            obj1.sum();
+            obj1.Avg();
+            obj1.largest();
+            obj1.OddEven();
+            obj1.reverse();
+            obj1.multiple();
+            obj1.search();
+            obj1.Sec_Large();
+            obj1.duplicate();
+            obj1.common();
+        }
+    }
+}
+
